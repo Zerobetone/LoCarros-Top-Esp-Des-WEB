a4fcedd7a5d3fb8eaa3b82cfb85c53a04e1fc99a
lucapwn <luscafter@gmail.com>
2022-03-26 14:39:10 -0300

    Added customer list screen

--- locarros/application/views.py
+++ locarros/application/views.py
@@ -1,7 +1,6 @@
 import re
 import json
 from datetime import datetime
-from textwrap import indent
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
