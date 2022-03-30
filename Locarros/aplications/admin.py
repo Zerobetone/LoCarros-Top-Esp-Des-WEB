bb851f087630008a6e1f4b3969c7d265766ee14e
Lucas Araújo <luscafter@gmail.com>
2022-03-30 15:48:06 -0300

    Delete Locarros directory

--- Locarros/aplications/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Veiculo, Locacao
-
-# Register your models here.
-admin.site.register(Veiculo)
-admin.site.register(Locacao)
